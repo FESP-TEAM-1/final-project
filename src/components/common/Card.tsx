1effeef107e548a621b6a3fc3c79d3c214f6456e
윤태현 <yoonth0919@gmail.com>
2023-11-20 17:57:05 +0900

    💄Design: Card 컴포넌트 마크업 및 스타일 (#1)
    Co-authored-by: 신은수<ShinEun9@users.noreply.github.com>
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>

---
+++ src/components/common/Card.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "styles/common/Card.module.css";
+
+interface CardItemType {
+  item: CardType;
+}
+
+interface CardType {
+  src: string;
+  title: string;
+  description: string;
+  publishedAt: string;
+  channelTitle: string;
+}
+
+// const Card = ({ item }: { item: CardType }) => {
+const Card: React.FC<CardItemType> = ({ item }) => {
+  const { src, channelTitle, title, description, publishedAt } = item;
+  return (
+    <>
+      <article className={styles["card"]}>
+        <div className={styles["card__cover"]}>
+          <img src={src} alt="" className={styles["card__cover__img"]} />
+        </div>
+        <p className={styles["card__channel-title"]}>{channelTitle}</p>
+        <h3 className={styles["card__title"]}>{title}</h3>
+        <p className={`${styles["card__description"]} ellipsis`}>
+          {description}
+        </p>
+        <p className={styles["card__published-at"]}>{publishedAt}</p>
+      </article>
+    </>
+  );
+};
+
+export default Card;
