1001f499a6e823105ea4235d05dab212c3d8cde9
윤태현 <yoonth0919@gmail.com>
2023-11-22 17:04:12 +0900

    💄 Design: Error 페이지 스타일 작업
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "pages/MainPage";
 import DetailPage from "pages/DetailPage";
+import ErrorPage from "pages/ErrorPage";
 import Root from "Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     id: "root",
-    // errorElement: <Error />,
+    errorElement: <ErrorPage />,
     element: <Root />,
     children: [
       {
