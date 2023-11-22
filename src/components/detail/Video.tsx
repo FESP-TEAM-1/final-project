9bba833d628c398040175b6df695179dbabd1b55
윤태현 <yoonth0919@gmail.com>
2023-11-22 15:45:35 +0900

    ✨ Feat: Video 컴포넌트 스타일 및 더보기 기능 추가 (#5)
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/components/detail/Video.tsx
+++ src/components/detail/Video.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useItemStore } from "stores/useItemStore";
 import styles from "styles/detail/Video.module.css";
+import decodeHTMLEntities from "utils/setDecodeHTMLEntities";
 
 interface VideoPropsType {
   videoId: string;
@@ -9,23 +10,46 @@ interface VideoPropsType {
 }
 
 const Video: React.FC<VideoPropsType> = ({ videoId, channelId }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const { itemInfo } = useItemStore();
+
   return (
-    <div>
-      <iframe
-        src={`https://www.youtube.com/embed/${videoId}`}
-        frameBorder="0"
-        allowFullScreen={true}
-      ></iframe>
-      <Link to={`/channel/${channelId}`}>
-        <p className={styles["card__channel-title"]}>{itemInfo.channelTitle}</p>
-      </Link>
-      <h3 className={`${styles["card__title"]} ellipsis-multi`}>
-        {itemInfo.title}
+    <div className={styles["video-container"]}>
+      <div className={styles["video-cover"]}>
+        <iframe
+          className={styles["video-iframe"]}
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
+          frameBorder="0"
+          allowFullScreen={true}
+        ></iframe>
+      </div>
+      <h3 className={styles["video-title"]}>
+        {decodeHTMLEntities(itemInfo.title)}
       </h3>
-      <p className={`${styles["card__description"]} ellipsis`}>
-        {itemInfo.description}
-      </p>
+      <Link to={`/channel/${channelId}`}>
+        <p className={styles["video-channel-title"]}>
+          {decodeHTMLEntities(itemInfo.channelTitle)}
+        </p>
+      </Link>
+      <div
+        className={`${styles["video-description"]} ${
+          !isOpen && styles["pointer"]
+        }`}
+        onClick={() => {
+          !isOpen && setIsOpen(true);
+        }}
+      >
+        <p
+          className={`${styles["video-description__content"]} ${
+            !isOpen && "ellipsis-multi"
+          }`}
+        >
+          {decodeHTMLEntities(itemInfo.description)}
+        </p>
+        <button type="button" onClick={() => setIsOpen(!isOpen)}>
+          {!isOpen ? "더보기" : "간략히"}
+        </button>
+      </div>
     </div>
   );
 };
