ebf17e813b680b4221573ddbe649e3df360a3142
ebokyung <dlqhrud303@naver.com>
2023-11-23 16:25:54 +0900

    ✨ Feat: 스켈레톤 및 컴포넌트 theme 색상 변경 적용 (#10)
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "components/layout/Header";
+import {useThemeStore} from "stores/useThemeStore";
 
 function Root() {
   const location = useLocation();
+  const {darkMode} = useThemeStore()
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    document.body.dataset.theme = darkMode ? 'dark' : 'light';
   }, [location]);
 
   return (
