c9decfb1c448fc673035df5636337fed6136bbdb
ShinEun9 <ses2201@naver.com>
2023-11-24 13:23:45 +0900

    ✨ Feat: 배경 클릭시 댓글팝업 제거 기능 추가 및 refactor(#5)

--- src/components/detail/Comment.tsx
+++ src/components/detail/Comment.tsx
@@ -1,58 +1,67 @@
-import React, { useState } from "react";
+import React from "react";
 import getElapsedTime from "utils/getElapsedTime";
-import {
-  CommentType,
-  SetActiveButtonIdType,
-  handleClickDeleteType,
-} from "types/commentItem";
+import { CommentType } from "types/commentItem";
+import { deleteCommentAPI } from "api/comment";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "styles/detail/Comment.module.css";
 
-interface newCommentType extends CommentType {
-  isActive: boolean;
-  setActiveCommentId: SetActiveButtonIdType;
-  handleClickDelete: handleClickDeleteType;
+interface CommmetPropsType {
+  item: CommentType;
+  activeCommentId: number | null;
+  setActiveCommentId: React.Dispatch<React.SetStateAction<number | null>>;
+  setComments: React.Dispatch<React.SetStateAction<CommentType[]>>;
 }
 
-const Comment: React.FC<newCommentType> = ({
-  id,
-  userId,
-  createdAt,
-  text,
-  isActive,
+const Comment: React.FC<CommmetPropsType> = ({
+  item,
+  activeCommentId,
   setActiveCommentId,
-  handleClickDelete,
+  setComments,
 }) => {
-  const handleClickEllipsis = () => {
-    setActiveCommentId(isActive ? null : id); // 현재 댓글의 상태를 토글
+  const handleClickDeleteComment = (id: number) => async () => {
+    const deleteCommmet = await deleteCommentAPI(id);
+    setComments((comments) =>
+      comments.filter((comment) => comment.id !== deleteCommmet.id)
+    );
   };
 
   return (
     <div className={styles["comment"]}>
       <div>
         <strong className={styles["comment__user"]}>
-          @user-{userId.split("-").at(-1)}
+          @user-{item.anonymous_user_id.split("-").at(-1)}
         </strong>
         <span className={styles["comment__created-at"]}>
-          {getElapsedTime(createdAt)}
+          {getElapsedTime(item.created_at)}
         </span>
-        <p className={styles["comment__text"]}>{text}</p>
+        <p className={styles["comment__text"]}>{item.text}</p>
       </div>
-      <FontAwesomeIcon
-        icon={faEllipsisVertical}
-        className={styles["comment__ellipsis"]}
-        onClick={handleClickEllipsis}
-      />
-      <div
-        className={styles["comment__button-list"]}
-        style={{ display: isActive ? "flex" : "none" }}
+
+      <button
+        type="button"
+        className={`${styles["comment__ellipsis"]} ${
+          item.id === activeCommentId && styles["active"]
+        }`}
+        onClick={() => setActiveCommentId(item.id)}
       >
-        <button type="button">신고</button>
-        <button type="button" onClick={() => handleClickDelete(id)}>
-          삭제
-        </button>
-      </div>
+        <FontAwesomeIcon icon={faEllipsisVertical} />
+      </button>
+
+      {item.id === activeCommentId && (
+        <>
+          <div className={styles["comment__button-list"]}>
+            <button type="button">신고</button>
+            <button type="button" onClick={handleClickDeleteComment(item.id)}>
+              삭제
+            </button>
+          </div>
+          <div
+            className={styles["dim"]}
+            onClick={() => setActiveCommentId(null)}
+          ></div>
+        </>
+      )}
     </div>
   );
 };
