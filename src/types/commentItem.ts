0cf89f000f4e657183e5d81d3e846afb453de6e1
ShinEun9 <ses2201@naver.com>
2023-11-24 13:10:42 +0900

    🔥 Remove: 불필요한 댓글 관련 type 제거 (#5)

--- src/types/commentItem.ts
+++ src/types/commentItem.ts
@@ -1,21 +1,7 @@
-export interface AllCommentType {
+export interface CommentType {
   anonymous_user_id: string;
   created_at: string;
   id: number;
   text: string | null;
   video_id: string;
 }
-
-export interface CommentType {
-  id: number;
-  userId: string;
-  createdAt: string;
-  text: string | null;
-}
-
-export type SetActiveButtonIdType = (id: number | null) => void;
-export type handleClickDeleteType = (id: number) => void;
-export type handleClickInsertType = (
-  videoId: string,
-  commentInput: string
-) => void;
