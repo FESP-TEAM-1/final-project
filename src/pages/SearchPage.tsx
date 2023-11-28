9fb89c5801575ace1dc443575cd9cba799d128a3
윤태현 <yoonth0919@gmail.com>
2023-11-28 17:13:38 +0900

    ✨ Feat: Search 페이지 유튜브 API 연결 및 무한 스크롤 구현 (#12, #21)
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,38 +1,43 @@
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import SearchCard from "components/search/SearchCard";
 import SearchSkeleton from "components/search/SearchSkeleton";
-import { YoutubeItem } from "types/mainItem";
 import { getSearchVideoListAPI } from "api/search";
 import styles from "styles/search/SearchPage.module.css";
+import useHandleScroll from "hooks/useHandleScroll";
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
 
   const {
-    isFetching,
-    error,
+    status,
     data: youtubeData,
-    refetch,
-  } = useQuery({
-    queryKey: ["youtubeSearch"],
-    queryFn: () => getSearchVideoListAPI(searchParams.get("title")!),
+    error,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["youtubeSearch", searchParams.get("title")!],
+    queryFn: ({ pageParam }) =>
+      getSearchVideoListAPI(searchParams.get("title")!, pageParam),
+    initialPageParam: "",
+    getNextPageParam: (lastPage) => lastPage.nextPageToken || undefined,
   });
 
-  useEffect(() => {
-    refetch();
-  }, [searchParams]);
+  useHandleScroll(fetchNextPage);
 
-  if (isFetching) return <SearchSkeleton />;
-  if (error) return <>{"An error has occurred: " + error.message}</>;
-  if (!youtubeData.length) return <>검색 결과가 없습니다!</>;
+  if (status === "pending") return <SearchSkeleton />;
+  if (status === "error")
+    return <>{"An error has occurred: " + error.message}</>;
+  if (youtubeData.pages[0].items.length === 0)
+    return <>검색 결과가 없습니다!</>;
 
   return (
     <main className={styles.main}>
-      {youtubeData.map((item: YoutubeItem) => {
-        return <SearchCard key={item.id} item={item} />;
-      })}
+      {youtubeData.pages
+        .flatMap((page) => page.items)
+        .map((item, i) => {
+          return <SearchCard key={i} item={item} />;
+        })}
     </main>
   );
 };
