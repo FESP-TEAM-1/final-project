a0d33bbdd205450b88e78ae5ed3ad3cb062c74ca
윤태현 <yoonth0919@gmail.com>
2023-11-29 14:57:59 +0900

    🔥 Remove: Detail 페이지 안 쓰는 import 지우기 (#5)
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,13 +1,9 @@
 // TODO: video id 유효값 조회 후 notfound 페이지 전환
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import VideoSection from "components/detail/VideoSection";
 import CommentSection from "components/detail/CommentSection";
 import RelatedSection from "components/detail/RelatedSection";
-import DetailSkeleton from "components/detail/DetailSkeleton";
-import { ChannelData } from "types/detailItem";
 import styles from "styles/detail/DetailPage.module.css";
 
 const DetailPage: React.FC = () => {
