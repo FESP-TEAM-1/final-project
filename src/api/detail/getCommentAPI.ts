4c41242e8cc87dccc17ea7775a4556e9b00e2e32
윤태현 <yoonth0919@gmail.com>
2023-11-29 14:55:37 +0900

    ✨ Feat: Comment 컴포넌트 더보기 버튼 구현 (#5, #21)
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/api/detail/getCommentAPI.ts
+++ src/api/detail/getCommentAPI.ts
@@ -1,11 +1,29 @@
 import { COMMENT_TABLE, supabase } from "supabase/videoComment";
 
-export const getCommentAPI = async (videoId: string) => {
-  const { data } = await supabase
-    .from(COMMENT_TABLE)
-    .select()
-    .eq("video_id", videoId)
-    .order("created_at", { ascending: false });
+export const getCommentAPI = async (videoId: string, pageParam: number) => {
+  const limit = 10;
+  const start = pageParam * limit;
+  const end = (pageParam + 1) * limit - 1;
 
-  return data;
+  const response = await supabase
+    .from(COMMENT_TABLE)
+    .select("*", { count: "exact" })
+    .eq("video_id", videoId)
+    .order("created_at", { ascending: false })
+    .range(start, end);
+
+  const data = response.data || [];
+  const totalCount = response.count;
+  const nextPageToken = data.length === limit ? pageParam + 1 : undefined;
+
+  const newDic = {
+    items: data,
+    nextPageToken,
+    pageInfo: {
+      totalResults: totalCount,
+      resultsPerPage: data.length,
+    },
+  };
+
+  return newDic;
 };
