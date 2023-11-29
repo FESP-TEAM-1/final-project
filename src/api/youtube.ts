d930ccc3ffb9fac07be399517f4361c56e55a346
ebokyung <dlqhrud303@naver.com>
2023-11-29 20:45:44 +0900

    ➕ Add: 개발/운영 클라이언트 API 클래스 생성 및 의존성 주입 (#28)
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>
    Co-authored-by: 윤태현 <yoonth95@users.noreply.github.com>

---
+++ src/api/youtube.ts
@@ -0,0 +1,87 @@
+import FakeYoutubeClient from "api/fakeYoutubeClient";
+import YoutubeClient from "api/youtubeClient";
+import { AxiosResponse } from "axios";
+import { COMMENT_TABLE, supabase } from "api/supabase/videoComment";
+
+export default class Youtube {
+  apiClient: FakeYoutubeClient | YoutubeClient;
+  constructor(apiClient: FakeYoutubeClient | YoutubeClient) {
+    this.apiClient = apiClient;
+  }
+
+  // 영상 한 개 조회
+  async getVideo(videoId: string, channelId: string) {
+    return this.apiClient.getVideoAPI(videoId, channelId);
+  }
+
+  // 인기 영상 리스트 조회
+  async getVideoList(pageParam: string) {
+    return this.apiClient
+      .getVideoListAPI(pageParam)
+      .then((res: AxiosResponse) => res.data);
+  }
+
+  // 채널 영상 리스트 조회
+  async getChannelData(channelId: string, pageParam: string) {
+    return this.apiClient
+      .getChannelDataAPI(channelId, pageParam)
+      .then((res: AxiosResponse) => res.data);
+  }
+
+  // 검색 영상 리스트 조회
+  async getSearchVideoList(title: string, pageParam: string) {
+    return this.apiClient.getSearchVideoListAPI(title, pageParam);
+  }
+
+  // 댓글 조회
+  async getComment(videoId: string, pageParam: number) {
+    const limit = 10;
+    const start = pageParam * limit;
+    const end = (pageParam + 1) * limit - 1;
+
+    const response = await supabase
+      .from(COMMENT_TABLE)
+      .select("*", { count: "exact" })
+      .eq("video_id", videoId)
+      .order("created_at", { ascending: false })
+      .range(start, end);
+
+    const data = response.data || [];
+    const totalCount = response.count;
+    const nextPageToken = data.length === limit ? pageParam + 1 : undefined;
+
+    const newDic = {
+      items: data,
+      nextPageToken,
+      pageInfo: {
+        totalResults: totalCount,
+        resultsPerPage: data.length,
+      },
+    };
+
+    return newDic;
+  }
+
+  // 댓글 추가
+  async insertComment(insertData: { videoId: string; commentInput: string }) {
+    const { commentInput, videoId } = insertData;
+
+    const { data } = await supabase
+      .from(COMMENT_TABLE)
+      .insert({ text: commentInput, video_id: videoId })
+      .select();
+
+    return data;
+  }
+
+  // 댓글 삭제
+  async deleteComment(id: number) {
+    const { data } = await supabase
+      .from(COMMENT_TABLE)
+      .delete()
+      .eq("id", id)
+      .select();
+
+    return data;
+  }
+}
