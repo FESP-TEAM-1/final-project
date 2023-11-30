47214db64079bb95e1429d2862141d6aa8b37d63
윤태현 <yoonth0919@gmail.com>
2023-11-30 13:46:46 +0900

    🐛 Fix: 누락 된 import 추가
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import SearchCard from "components/search/SearchCard";
